d1abba3b549f8c456c37f13321d6f6ff115db0d8
honeydewdrop <smadam26@g.holycross.edu>
2025-05-21 19:01:50 -0400

    fixed ipad/mobile formatting 2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       className="min-h-screen w-full bg-gradient-to-r from-pink-500 via-blue-200 to-green-200 text-white flex flex-col justify-center items-center relative"
     >
       <header
-        className="text-6xl font-Ubuntu text-center mb-6 text-pink-400"
+        className="text-6xl font-Ubuntu text-center mb-6 text-purple-400"
         style={{ zIndex: 1 }}
       >
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
@@ -75,28 +75,28 @@ const Home = () => {
         <i className="devicon-python-plain text-5xl mr-2"></i>
         <i class="devicon-django-plain text-5xl mr-2"></i>
         <i className="devicon-javascript-plain text-5xl mr-2"></i>
-        <i className="devicon-react-original text-5xl mr-2"></i>
-        <i class="devicon-csharp-plain text-5xl mr-2"></i>
-        <i class="devicon-dotnetcore-plain text-5xl mr-2"></i>
-        <i className="devicon-java-plain text-5xl"></i>
+        <i className="devicon-react-original text-5xl mr-1.5"></i>
+        <i class="devicon-csharp-plain text-5xl mr-1.5"></i>
+        <i class="devicon-dotnetcore-plain text-5xl mr-1.5"></i>
+        <i className="devicon-java-plain text-5xl" ></i>
       </div>
-      <div className="flex flex-row" style={{ zIndex: 1 }}>
-        <div className="w-56 h-56 overflow-x-hidden rounded-full mt-5">
+      <div className="flex flex-row w-100" style={{ zIndex: 1 }}>
+        <div className="w-70 h-72 size-50 overflow-x-hidden rounded-full mt-8 ml-2">
           <img
             src="/meportfolio.png"
             alt="Samantha Adams"
-            className="w-full h-full object-cover ml-2"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div
-          className="w-50 h-full overflow-hidden mt-5"
+        {/* <div
+          className="w-59 h-80 size-50 overflow-hidden mt-10"
           style={{ zIndex: 1 }}
         >
           <img
             src="/miliofigure.png"
             alt="Faerie Court Milio from League of Legends"
           />
-        </div>
+        </div> */}
       </div>
       <div className="text-gray-500 mt-5 ml-4 flex flex-row" style={{ zIndex: 0 }}>
         <a href="https://www.linkedin.com/in/samantha-adams-a17917166/" target="_blank" rel="noopener noreferrer">
